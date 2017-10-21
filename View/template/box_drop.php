c43d9bfb03a2fab122097899ec87cfe8422fcbc9
shinate <shine.wangrs@gmail.com>
2017-10-21 23:34:07 +0800

    remove exports

---
+++ View/template/box_drop.php
@@ -0,0 +1,10 @@
+# Tethealla Server Box Drop File
+# Episode: <?php echo $ep_disp; ?>, Section ID: <?php echo $sec_disp; ?>, Difficulty: <?php echo $dif_disp; ?>
+
+#
+# File layout:
+#
+# Two lines per enemy.
+# First line is rare occurance rate.  Valid values from 0 to 255.
+# Second line is hex for actual drop.  Refer to bb_items.txt for valid hex values.
+<?php echo $contents; ?>
