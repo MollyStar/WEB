ec24a32fbb48f4ec45100ecace5973a44c241d11
shinate <shine.wangrs@gmail.com>
2017-11-05 00:44:24 +0800

    空格问题

--- __SERVER/script/item_links.bat
+++ __SERVER/script/item_links.bat
@@ -5,7 +5,7 @@ del C:\data\www\pso.codante.org\__SERVER\item\shieldpmt.ini
 mklink C:\data\www\pso.codante.org\__SERVER\item\shieldpmt.ini C:\data\PSOBB\SERVER\config\ITEM\shieldpmt.ini
 
 del C:\data\www\pso.codante.org\__SERVER\item\tech.ini
-mklinkC:\data\www\pso.codante.org\__SERVER\item\tech.ini C:\data\PSOBB\SERVER\config\ITEM\tech.ini
+mklink C:\data\www\pso.codante.org\__SERVER\item\tech.ini C:\data\PSOBB\SERVER\config\ITEM\tech.ini
 
 del C:\data\www\pso.codante.org\__SERVER\item\weaponpmt.ini
 mklink C:\data\www\pso.codante.org\__SERVER\item\weaponpmt.ini C:\data\PSOBB\SERVER\config\ITEM\weaponpmt.ini
